6ee7e32a0b1a64ed84238506b397bc40201f6d60
gal_rabin <galrabin@gmail.com>
2019-06-01 20:01:24 +0300

    3

--- exercises/ex5/operationChain.c
+++ exercises/ex5/operationChain.c
@@ -1,34 +1,57 @@
-/*************************************************
-*  opratioChain.c
-*  ----------
-*  Main inplementation
-*************************************************/
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-/********************************/
-/**    FUNCTION DECLARATIONS   **/
-/********************************/
 
-/********************************/
-/**    FUNCTION DEFINITIONS    **/
-/********************************/
+#define MAX_NUMS 10
+#define MAX_INT_LEN 12
 
-/*********************************
-* Problem 3
-* Declare main function
-* returns exit code 0
-*********************************/
-// Compile === gcc -Wall intString.c intStringList.c  operationChain.c -o test_ex5_3
-// Test === test_ex5_3 , compare to = /share/ex_data/ex5/test_ex5_3.out
+char *intToStringList(const int *numArray, int arrayLen, char *buffer);
 
-int main()
-{
+int doOpChain(char *intStrList, char op, char *res_intStr);
+
+
+int main() {
+
     char op;
-    float n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
     printf("Please enter an operator (+ or *) followed by up to 10 integer numbers.\n");
-    scanf("%c %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", &n1, &n2, &n3, &n4, &n5, &n6, &n7, &n8, &n9, &n10);
-    printf("%lf", n1);
+    scanf("%c ", &op);
 
+    if (op != '+' && op != '*') {
+        printf("Invalid operator %c\n", op);
+        return 1;
+    }
+
+    double nums[MAX_NUMS];
+    int input, check, space, current;
+    for (int i = 0; i < MAX_NUMS; i++) {
+        check = scanf("%lf ", &nums[i]);
+        input = nums[i];
+        space = input - nums[i];
+        if (!input || space != 0) {
+            if (i == 0) {
+                printf("No valid numbers were entered");
+                return 1;
+            }
+            break;
+        }
+        current++;
+    }
+
+    int castNumsInt[current];
+
+    for (int i = 0; i < current; i++) {
+        castNumsInt[i] = nums[i];
+    }
+
+    char numsCastStrings[(MAX_INT_LEN * MAX_NUMS) + 1];
+    intToStringList(castNumsInt, current, numsCastStrings);
+
+    char resultString[20];
+    int res = doOpChain(numsCastStrings, op, resultString);
+    if (!res) {
+        printf("Result is out of bound\n");
+        return 1;
+    }
+    printf("= %s", resultString);
     return 0;
 }
